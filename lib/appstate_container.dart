236bf57cf967b8998805b17899a55d3d66ccabc5
yekta <ngyekta@gmail.com>
2019-05-30 03:55:31 +0300

    Theme name changed

--- lib/appstate_container.dart
+++ lib/appstate_container.dart
@@ -47,7 +47,7 @@ class StateContainer extends StatefulWidget {
 /// 
 /// Basically the central hub behind the entire app
 class StateContainerState extends State<StateContainer> {
-  BaseTheme curTheme = BlaiseTheme();
+  BaseTheme curTheme = BlaiseLightTheme();
   LanguageSetting curLanguage = LanguageSetting(AvailableLanguage.DEFAULT);
 
   @override
