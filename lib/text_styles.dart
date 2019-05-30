2dd7b75a20acb0e46231fcd70d9fd91d2e840a35
yekta <ngyekta@gmail.com>
2019-05-30 03:53:27 +0300

    Custom icon font added

--- lib/text_styles.dart
+++ lib/text_styles.dart
@@ -16,10 +16,7 @@ class AppFontSizes {
   static const smallest = 12.0;
   static const small = 14.0;
   static const medium = 16.0;
-  static const _large = 20.0;
+  static const large = 20.0;
   static const larger = 24.0;
-  static const _largest = 28.0;
-  static const largestc = 28.0;
-  static const _sslarge = 18.0;
-  static const _sslargest = 22.0;
+  static const largest = 28.0;
 }
