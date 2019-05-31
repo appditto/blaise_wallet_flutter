530cdb1411dbd738993bd3c21e90d2aea49b566f
yekta <ngyekta@gmail.com>
2019-05-31 05:05:54 +0300

    Outline button fixed

--- lib/ui/widgets/buttons.dart
+++ lib/ui/widgets/buttons.dart
@@ -18,7 +18,12 @@ class AppButton extends StatefulWidget {
   final bool disabled;
   final bool buttonTop;
 
-  AppButton({this.type, this.text, this.onPressed, this.disabled = false, this.buttonTop = false});
+  AppButton(
+      {this.type,
+      this.text,
+      this.onPressed,
+      this.disabled = false,
+      this.buttonTop = false});
 
   _AppButtonState createState() => _AppButtonState();
 }
@@ -27,51 +32,134 @@ class _AppButtonState extends State<AppButton> {
   @override
   Widget build(BuildContext context) {
     return Expanded(
-      child: Container(
-        decoration: widget.type == AppButtonType.Primary || widget.type == AppButtonType.Success ? BoxDecoration(
-          borderRadius: BorderRadius.circular(12.0),
-          gradient: widget.type == AppButtonType.Primary ? StateContainer.of(context).curTheme.gradientPrimary : StateContainer.of(context).curTheme.gradientPrimary, // Success color placeholder
-          boxShadow: [
-            StateContainer.of(context).curTheme.shadowPrimaryOne,
-          ],
-        ) 
-        : BoxDecoration(
-              borderRadius: BorderRadius.circular(12),
-              boxShadow: [
-                 widget.type == AppButtonType.PrimaryOutline ? StateContainer.of(context).curTheme.shadowPrimaryTwo : StateContainer.of(context).curTheme.shadowPrimaryTwo,
-              ]),
-        margin: widget.buttonTop
-            ? EdgeInsetsDirectional.fromSTEB(20, 20, 20, 0)
-            : EdgeInsetsDirectional.fromSTEB(
-                20,
-                16,
-                20,
-                (MediaQuery.of(context).padding.bottom) +
-                    (24 - (MediaQuery.of(context).padding.bottom) / 2),
+      child: widget.type == AppButtonType.Primary
+          // Primary Button
+          ? Container(
+              decoration: widget.type == AppButtonType.Primary ||
+                      widget.type == AppButtonType.Success
+                  ? BoxDecoration(
+                      borderRadius: BorderRadius.circular(12.0),
+                      gradient: widget.type == AppButtonType.Primary
+                          ? StateContainer.of(context).curTheme.gradientPrimary
+                          : StateContainer.of(context)
+                              .curTheme
+                              .gradientPrimary, // Success color placeholder
+                      boxShadow: [
+                        StateContainer.of(context).curTheme.shadowPrimaryOne,
+                      ],
+                    )
+                  : BoxDecoration(
+                      borderRadius: BorderRadius.circular(12),
+                      boxShadow: [
+                          StateContainer.of(context).curTheme.shadowPrimaryTwo,
+                        ]),
+              margin: widget.buttonTop
+                  ? EdgeInsetsDirectional.fromSTEB(20, 20, 20, 0)
+                  : EdgeInsetsDirectional.fromSTEB(
+                      20,
+                      16,
+                      20,
+                      (MediaQuery.of(context).padding.bottom) +
+                          (24 - (MediaQuery.of(context).padding.bottom) / 2),
+                    ),
+              height: 50,
+              child: FlatButton(
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(12.0)),
+                child: AutoSizeText(
+                  widget.text,
+                  textAlign: TextAlign.center,
+                  maxLines: 1,
+                  stepGranularity: 0.1,
+                  style: AppStyles.buttonPrimary(context),
+                ),
+                splashColor:
+                    StateContainer.of(context).curTheme.backgroundPrimary30,
+                highlightColor: StateContainer.of(context).curTheme.primary15,
+                onPressed: () {
+                  if (widget.onPressed != null && !widget.disabled) {
+                    widget.onPressed();
+                  }
+                  return;
+                },
               ),
-        height: 50,
-        child: FlatButton(
-          shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(12.0)),
-          child: AutoSizeText(
-            widget.text,
-            textAlign: TextAlign.center,
-            maxLines: 1,
-            stepGranularity: 0.1,
-            style: widget.type == AppButtonType.Primary ? AppStyles.buttonPrimary(context) : AppStyles.buttonPrimaryOutline(context),
-          ),
-          splashColor:
-              widget.type == AppButtonType.Primary ? StateContainer.of(context).curTheme.backgroundPrimary30 : StateContainer.of(context).curTheme.primary30,
-          highlightColor:
-              widget.type == AppButtonType.Primary ? StateContainer.of(context).curTheme.backgroundPrimary15 : StateContainer.of(context).curTheme.primary15,
-          onPressed: () {
-            if (widget.onPressed != null && !widget.disabled) {
-              widget.onPressed();
-            }
-            return;
-          },
-        ),
-      ),
+            )
+          // Outlined Button
+          : Container(
+              margin: widget.buttonTop
+                  ? EdgeInsetsDirectional.fromSTEB(20, 20, 20, 0)
+                  : EdgeInsetsDirectional.fromSTEB(
+                      20,
+                      16,
+                      20,
+                      (MediaQuery.of(context).padding.bottom) +
+                          (24 - (MediaQuery.of(context).padding.bottom) / 2),
+                    ),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(12),
+                boxShadow: [
+                  StateContainer.of(context).curTheme.shadowPrimaryTwo,
+                ],
+              ),
+              child: Stack(
+                children: <Widget>[
+                  Stack(
+                    children: <Widget>[
+                      Container(
+                        width: double.maxFinite,
+                        height: 50,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(12),
+                          gradient: StateContainer.of(context)
+                              .curTheme
+                              .gradientPrimary,
+                        ),
+                      ),
+                      Container(
+                        margin: EdgeInsets.all(2),
+                        height: 46,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(10),
+                          color: StateContainer.of(context)
+                              .curTheme
+                              .backgroundPrimary,
+                        ),
+                      ),
+                    ],
+                  ),
+                  Container(
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(12),
+                      color: Colors.transparent,
+                    ),
+                    height: 50,
+                    width: double.maxFinite,
+                    child: FlatButton(
+                      child: AutoSizeText(
+                        widget.text,
+                        textAlign: TextAlign.center,
+                        maxLines: 1,
+                        stepGranularity: 0.1,
+                        style: AppStyles.buttonPrimaryOutline(context),
+                      ),
+                      color: Colors.transparent,
+                      splashColor:
+                          StateContainer.of(context).curTheme.primary30,
+                      highlightColor:
+                          StateContainer.of(context).curTheme.primary15,
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(12.0)),
+                      onPressed: () {
+                        if (widget.onPressed != null && !widget.disabled) {
+                          widget.onPressed();
+                        }
+                        return;
+                      },
+                    ),
+                  ),
+                ],
+              ),
+            ),
     );
   }
 }
