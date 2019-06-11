ce36b017a6cde38354f43ccaf83f5718f740bfa1
Brandon Berhent <bbedward@gmail.com>
2019-06-11 13:31:58 -0400

    Fix TapOutsideUnfocus

--- lib/ui/settings/backup_private_key/encrypt_private_key_sheet.dart
+++ lib/ui/settings/backup_private_key/encrypt_private_key_sheet.dart
@@ -36,192 +36,194 @@ class _EncryptPrivateKeySheetState extends State<EncryptPrivateKeySheet> {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: <Widget>[
-        Expanded(
-          child: Container(
-            decoration: BoxDecoration(
-              color: StateContainer.of(context).curTheme.backgroundPrimary,
-              borderRadius: BorderRadius.only(
-                topLeft: Radius.circular(12),
-                topRight: Radius.circular(12),
-              ),
-            ),
-            child: Column(
-              children: <Widget>[
-                // Sheet header
-                Container(
-                  height: 60,
-                  width: double.maxFinite,
-                  decoration: BoxDecoration(
-                    gradient:
-                        StateContainer.of(context).curTheme.gradientPrimary,
-                    borderRadius: BorderRadius.only(
-                      topLeft: Radius.circular(12),
-                      topRight: Radius.circular(12),
-                    ),
-                  ),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: <Widget>[
-                      // Close Button
-                      Container(
-                        margin: EdgeInsetsDirectional.only(start: 5, end: 10),
-                        height: 50,
-                        width: 50,
-                        child: FlatButton(
-                            highlightColor:
-                                StateContainer.of(context).curTheme.textLight15,
-                            splashColor:
-                                StateContainer.of(context).curTheme.textLight30,
-                            onPressed: () {
-                              Navigator.pop(context);
-                            },
-                            shape: RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(50.0)),
-                            padding: EdgeInsets.all(0.0),
-                            child: Icon(AppIcons.close,
-                                color: StateContainer.of(context)
-                                    .curTheme
-                                    .textLight,
-                                size: 20)),
-                      ),
-                      // Header
-                      Container(
-                        width: MediaQuery.of(context).size.width - 130,
-                        alignment: Alignment(0, 0),
-                        child: AutoSizeText(
-                          "ENCRYPT",
-                          style: AppStyles.header(context),
-                          maxLines: 1,
-                          stepGranularity: 0.1,
-                          textAlign: TextAlign.center,
-                        ),
-                      ),
-                      // Sized Box
-                      SizedBox(
-                        height: 50,
-                        width: 65,
-                      ),
-                    ],
-                  ),
+    return TapOutsideUnfocus(
+      child: Column(
+        children: <Widget>[
+          Expanded(
+            child: Container(
+              decoration: BoxDecoration(
+                color: StateContainer.of(context).curTheme.backgroundPrimary,
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(12),
+                  topRight: Radius.circular(12),
                 ),
-                Expanded(
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: <Widget>[
-                      // Paragraph
-                      Container(
-                        width: double.maxFinite,
-                        margin: EdgeInsetsDirectional.fromSTEB(30, 30, 30, 20),
-                        child: AutoSizeText(
-                          "Create a new password to encrypt the your private key.",
-                          style: AppStyles.paragraph(context),
-                          stepGranularity: 0.1,
-                          maxLines: 3,
-                          minFontSize: 8,
-                        ),
+              ),
+              child: Column(
+                children: <Widget>[
+                  // Sheet header
+                  Container(
+                    height: 60,
+                    width: double.maxFinite,
+                    decoration: BoxDecoration(
+                      gradient:
+                          StateContainer.of(context).curTheme.gradientPrimary,
+                      borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(12),
+                        topRight: Radius.circular(12),
                       ),
-                      Expanded(
-                        child: KeyboardAvoider(
-                          duration: Duration(milliseconds: 0),
-                          autoScroll: true,
-                          focusPadding: 40,
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: <Widget>[
-                              // Container for new password field
-                              Container(
-                                margin: EdgeInsetsDirectional.fromSTEB(
-                                    30, 10, 30, 0),
-                                child: AppTextField(
-                                  label: 'New Password',
-                                  style: AppStyles.paragraphMedium(context),
-                                  maxLines: 1,
-                                  passwordField: true,
-                                  focusNode: passwordFocusNode,
-                                  controller: passwordController,
-                                  onChanged: (String newText) {
-                                    if (passwordError != null) {
-                                      setState(() {
-                                        passwordError = null;
-                                      });
-                                    }
-                                  },
-                                ),
-                              ),
-                              // Container for confirm password field
-                              Container(
-                                margin: EdgeInsetsDirectional.fromSTEB(
-                                    30, 30, 30, 40),
-                                child: AppTextField(
-                                  label: 'Confirm',
-                                  style: AppStyles.paragraphMedium(context),
-                                  maxLines: 1,
-                                  passwordField: true,
-                                  focusNode: confirmPasswordFocusNode,
-                                  controller: confirmPasswordController,
-                                  onChanged: (String newText) {
-                                    if (passwordError != null) {
-                                      setState(() {
-                                        passwordError = null;
-                                      });
-                                    }
-                                    if (confirmPasswordController.text == passwordController.text) {
-                                      confirmPasswordFocusNode.unfocus();
-                                      passwordFocusNode.unfocus();
-                                    }                                    
-                                  },
-                                ),
-                              ),
-                              // Error text
-                              Container(
-                                margin: EdgeInsetsDirectional.only(
-                                    start: 30, end: 30, top: 4, bottom: 40),
-                                child: AutoSizeText(
-                                  passwordError == null ? "" : passwordError,
-                                  style:
-                                      AppStyles.paragraphPrimary(context),
-                                  textAlign: TextAlign.start,
-                                ),
-                              )
-                            ],
+                    ),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: <Widget>[
+                        // Close Button
+                        Container(
+                          margin: EdgeInsetsDirectional.only(start: 5, end: 10),
+                          height: 50,
+                          width: 50,
+                          child: FlatButton(
+                              highlightColor:
+                                  StateContainer.of(context).curTheme.textLight15,
+                              splashColor:
+                                  StateContainer.of(context).curTheme.textLight30,
+                              onPressed: () {
+                                Navigator.pop(context);
+                              },
+                              shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(50.0)),
+                              padding: EdgeInsets.all(0.0),
+                              child: Icon(AppIcons.close,
+                                  color: StateContainer.of(context)
+                                      .curTheme
+                                      .textLight,
+                                  size: 20)),
+                        ),
+                        // Header
+                        Container(
+                          width: MediaQuery.of(context).size.width - 130,
+                          alignment: Alignment(0, 0),
+                          child: AutoSizeText(
+                            "ENCRYPT",
+                            style: AppStyles.header(context),
+                            maxLines: 1,
+                            stepGranularity: 0.1,
+                            textAlign: TextAlign.center,
                           ),
                         ),
+                        // Sized Box
+                        SizedBox(
+                          height: 50,
+                          width: 65,
+                        ),
+                      ],
+                    ),
+                  ),
+                  Expanded(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        // Paragraph
+                        Container(
+                          width: double.maxFinite,
+                          margin: EdgeInsetsDirectional.fromSTEB(30, 30, 30, 20),
+                          child: AutoSizeText(
+                            "Create a new password to encrypt the your private key.",
+                            style: AppStyles.paragraph(context),
+                            stepGranularity: 0.1,
+                            maxLines: 3,
+                            minFontSize: 8,
+                          ),
+                        ),
+                        Expanded(
+                          child: KeyboardAvoider(
+                            duration: Duration(milliseconds: 0),
+                            autoScroll: true,
+                            focusPadding: 40,
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: <Widget>[
+                                // Container for new password field
+                                Container(
+                                  margin: EdgeInsetsDirectional.fromSTEB(
+                                      30, 10, 30, 0),
+                                  child: AppTextField(
+                                    label: 'New Password',
+                                    style: AppStyles.paragraphMedium(context),
+                                    maxLines: 1,
+                                    passwordField: true,
+                                    focusNode: passwordFocusNode,
+                                    controller: passwordController,
+                                    onChanged: (String newText) {
+                                      if (passwordError != null) {
+                                        setState(() {
+                                          passwordError = null;
+                                        });
+                                      }
+                                    },
+                                  ),
+                                ),
+                                // Container for confirm password field
+                                Container(
+                                  margin: EdgeInsetsDirectional.fromSTEB(
+                                      30, 30, 30, 40),
+                                  child: AppTextField(
+                                    label: 'Confirm',
+                                    style: AppStyles.paragraphMedium(context),
+                                    maxLines: 1,
+                                    passwordField: true,
+                                    focusNode: confirmPasswordFocusNode,
+                                    controller: confirmPasswordController,
+                                    onChanged: (String newText) {
+                                      if (passwordError != null) {
+                                        setState(() {
+                                          passwordError = null;
+                                        });
+                                      }
+                                      if (confirmPasswordController.text == passwordController.text) {
+                                        confirmPasswordFocusNode.unfocus();
+                                        passwordFocusNode.unfocus();
+                                      }                                    
+                                    },
+                                  ),
+                                ),
+                                // Error text
+                                Container(
+                                  margin: EdgeInsetsDirectional.only(
+                                      start: 30, end: 30, top: 4, bottom: 40),
+                                  child: AutoSizeText(
+                                    passwordError == null ? "" : passwordError,
+                                    style:
+                                        AppStyles.paragraphPrimary(context),
+                                    textAlign: TextAlign.start,
+                                  ),
+                                )
+                              ],
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  // "Encrypt" button
+                  Row(
+                    children: <Widget>[
+                      AppButton(
+                        type: AppButtonType.Primary,
+                        text: "Encrypt",
+                        buttonTop: true,
+                        onPressed: () {
+                          validatePasswordMatchAndEncrypt();
+                        },
                       ),
                     ],
                   ),
-                ),
-                // "Encrypt" button
-                Row(
-                  children: <Widget>[
-                    AppButton(
-                      type: AppButtonType.Primary,
-                      text: "Encrypt",
-                      buttonTop: true,
-                      onPressed: () {
-                        validatePasswordMatchAndEncrypt();
-                      },
-                    ),
-                  ],
-                ),
-                // "Close" button
-                Row(
-                  children: <Widget>[
-                    AppButton(
-                      type: AppButtonType.PrimaryOutline,
-                      text: "Close",
-                      onPressed: () {
-                        Navigator.pop(context);
-                      },
-                    ),
-                  ],
-                ),
-              ],
+                  // "Close" button
+                  Row(
+                    children: <Widget>[
+                      AppButton(
+                        type: AppButtonType.PrimaryOutline,
+                        text: "Close",
+                        onPressed: () {
+                          Navigator.pop(context);
+                        },
+                      ),
+                    ],
+                  ),
+                ],
+              ),
             ),
-          ),
-        )
-      ],
+          )
+        ],
+      )
     );
   }
 
--- lib/ui/widgets/tap_outside_unfocus.dart
+++ lib/ui/widgets/tap_outside_unfocus.dart
@@ -1,31 +1,20 @@
 import 'package:flutter/material.dart';
 
-/// A widget that will unfocus any focus nodes when tapped outside of the child boundaries
+/// Simple wrapper that will clear focus when a tap is detected outside its boundaries
 class TapOutsideUnfocus extends StatelessWidget {
-  final List<FocusNode> focusNodes;
   final Widget child;
 
-  TapOutsideUnfocus({@required this.focusNodes, @required this.child});
+  TapOutsideUnfocus({@required this.child});
 
   @override
   Widget build(BuildContext context) {
-    return Stack(
-      children: <Widget>[
-        GestureDetector(
-          onTap: () {
-            // Clear focus of our fields when tapped in this empty space
-            focusNodes.forEach((f) {
-              f.unfocus();
-            });
-          },
-          child: Container(
-            color: Colors.transparent,
-            child: SizedBox.expand(),
-            constraints: BoxConstraints.expand(),
-          ),
-        ),
-        this.child
-      ]
-    );
-  }
+    return 
+      GestureDetector(
+        onTap: () {
+          // Clear focus of our fields when tapped in this empty space
+          FocusScope.of(context).detach();
+        },
+        child: this.child
+      );
+}
 }
