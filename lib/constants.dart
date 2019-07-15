ee72f185391fde818810628922759d22afe4f60b
Brandon Berhent <bbedward@gmail.com>
2019-07-15 13:51:50 -0400

    Add EULA and Privacy policy strings

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,4 +1,6 @@
 class AppConstants {
   static const String DEFAULT_RPC_HTTP_URL =
       'https://blaise.apptalos.com/rawrpc';
+  static const String EULA_URL = 'https://appditto.github.io/blaise/eula.html';
+  static const String PRIVACY_POLICY_URL = 'https://appditto.github.io/blaise/privacy_policy.html';
 }
