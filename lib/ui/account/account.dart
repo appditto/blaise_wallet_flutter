509116acfbb6a9ca6c257658e6f9147c0414d631
yekta <ngyekta@gmail.com>
2019-08-09 19:12:32 +0300

    Small change

--- lib/ui/account/account.dart
+++ lib/ui/account/account.dart
@@ -415,7 +415,10 @@ class _AccountPageState extends State<AccountPage>
                           child: Material(
                             color: Colors.transparent,
                             child: InkWell(
-                              onTap: () {
+                              onTap:widget.account.isBorrowed
+                                    ? (){
+                                      return null;
+                                    }:() {
                                 showAppDialog(
                                     context: context,
                                     builder: (_) => DialogOverlay(
