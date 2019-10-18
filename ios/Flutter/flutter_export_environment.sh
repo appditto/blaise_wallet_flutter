8d431568ecdadc614374295cc22cefedba1800aa
yekta <ngyekta@gmail.com>
2019-10-18 16:42:48 +0300

    Update version to 1.0.3

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -2,10 +2,9 @@
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=/Users/yekta/flutter/flutter"
 export "FLUTTER_APPLICATION_PATH=/Users/yekta/blaise_wallet_flutter"
-export "FLUTTER_TARGET=/Users/yekta/blaise_wallet_flutter/lib/main.dart"
+export "FLUTTER_TARGET=lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "SYMROOT=${SOURCE_ROOT}/../build/ios"
-export "FLUTTER_FRAMEWORK_DIR=/Users/yekta/flutter/flutter/bin/cache/artifacts/engine/ios"
+export "FLUTTER_FRAMEWORK_DIR=/Users/yekta/flutter/flutter/bin/cache/artifacts/engine/ios-release"
 export "FLUTTER_BUILD_NAME=1.0.2"
 export "FLUTTER_BUILD_NUMBER=12"
-export "TRACK_WIDGET_CREATION=true"
